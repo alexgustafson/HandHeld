f919cdda199f115e05b67b4640db4f1ff5f2cd98
Alex Gustafson <alexg.hsz@gmail.com>
2012-12-05 16:11:16 +0100

    new db structure

---
+++ Web/application/models/contenttype_model.php
@@ -0,0 +1,13 @@
+<?php
+
+class Contenttype_model extends CI_Model
+{
+  var $type;
+  var $isComposite;
+
+  function __construct()
+  {
+    parent::__construct();
+  }
+
+}
---
+++ Web/application/models/fieldtype_model.php
@@ -0,0 +1,18 @@
+<?php
+  /**
+   * Created by JetBrains PhpStorm.
+   * User: alex_gustafson
+   * Date: 05.12.12
+   * Time: 16:09
+   * To change this template use File | Settings | File Templates.
+   */
+  class Fieldtype_model extends CI_Model
+  {
+    var $name;
+
+    function __construct()
+    {
+      parent::__construct();
+    }
+
+  }

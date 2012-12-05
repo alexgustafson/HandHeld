86a593afa43f26de5b6f0488754f80724b2d587e
Alex Gustafson <alexg.hsz@gmail.com>
2012-12-05 17:39:23 +0100

    new db structure

--- Web/application/models/fieldtype_model.php
+++ Web/application/models/fieldtype_model.php
@@ -13,6 +13,17 @@
     function __construct()
     {
       parent::__construct();
+      $this->load->database();
+    }
+
+    public function getFieldsForContentType($content_type_id)
+    {
+      $this->db->select('*');
+      $this->db->from('fields');
+      $this->db->where('content_type_id', $content_type_id);
+      $query = $this->db->get();
+      $data = $query->resutl();
+      return $data;
     }
 
   }

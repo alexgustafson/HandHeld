d2f2daf974f6434d386c426f14d45fb889e32a6e
Alex Gustafson <alexg.hsz@gmail.com>
2012-12-16 14:30:08 +0100

    optimus_dashboard css framework

---
+++ Web/application/views/templates/leftmenu.php
@@ -0,0 +1,15 @@
+<div class="container-fluid">
+    <div class="row-fluid">
+
+        <!-- start: Main Menu -->
+        <div class="span2 main-menu-span">
+            <div class="nav-collapse sidebar-nav">
+                <ul class="nav nav-tabs nav-stacked main-menu">
+                    <li class="nav-header hidden-tablet">Navigation</li>
+                    <li><a href="index.php"><i class="icon-home"></i><span class="hidden-tablet"> Dashboard</span></a></li>
+                    <li><a href="ui.html"><i class="icon-briefcase"></i><span class="hidden-tablet"> Document</span></a></li>
+
+                </ul>
+            </div><!--/.well -->
+        </div><!--/span-->
+        <!-- end: Main Menu -->
